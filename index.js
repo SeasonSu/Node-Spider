4e87ed598b9080fdd28395c99cfd039520e65d51
season <hsu@ewaytec.cn>
2018-01-12 16:58:47 +0800

    12

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const utils = require('./utils')
 const args = process.argv.splice(2)
+
+async function get(){
+    let url = await utils.getWechatUrl('南方都市报')
+    console.log(url)
+}
+
+get()
--- utils.js
+++ utils.js
@@ -1,22 +1,6 @@
 const request = require('request')
 const cheerio = require('cheerio')
 
-var getUrl =  async function(name,callback) {
-    let encode_name = encodeURIComponent(name)
-    let url = `http://weixin.sogou.com/weixin?type=1&query=${name}&ie=utf8&_sug_=y&_sug_type_=1`
-    return await request(url, function (err, response, html) {
-        if (err) return callback(err, null);
-        if (html.indexOf('<title>302 Found</title>') != -1) return callback(null, '302');
-        if (html.indexOf('您的访问过于频繁') != -1) return callback('-访问过于频繁')
-        var $ = cheerio.load(html);
-        //公众号页面的临时url
-        var wechat_url = $($("#sogou_vr_11002301_box_0 a")[0]).attr('href') || '';
-        var tg = wechat_url.replace(/amp;/g, '')
-        console.log(tg)
-        return tg
-    })
-}
-
 const utils = {
     async promise(callback){
         return new Promise(function (resolve, reject) {
@@ -25,32 +9,21 @@ const utils = {
     },
     async getWechatUrl(name,callback){
         let encode_name = encodeURIComponent(name)
-
-        let url = `http://weixin.sogou.com/weixin?type=1&query=${name}&ie=utf8&_sug_=y&_sug_type_=1`
-        console.log(url)
-        return await request(url, function (err, response, html) {
-            if (err) return callback(err, null);
-            if (html.indexOf('<title>302 Found</title>') != -1) return callback(null, '302');
-            if (html.indexOf('您的访问过于频繁') != -1) return callback('-访问过于频繁')
-            var $ = cheerio.load(html);
-            //公众号页面的临时url
-            var wechat_url = $($("#sogou_vr_11002301_box_0 a")[0]).attr('href') || '';
-            var tg = wechat_url.replace(/amp;/g, '')
-            setTimeout(function () {
-              console.log(tg)
-            }, 1000 + Math.ceil(Math.random() * 500));
-            return tg
+        let url = `http://weixin.sogou.com/weixin?type=1&query=${encode_name}&ie=utf8&_sug_=y&_sug_type_=1`
+        return await utils.promise(async function(resolve){
+            await request(url, function (err, response, html) {
+                if (err) return callback(err, null)
+                if (html.indexOf('<title>302 Found</title>') != -1) return callback(null, '302')
+                if (html.indexOf('您的访问过于频繁') != -1) return callback('-访问过于频繁')
+                let $ = cheerio.load(html)
+                let wechat_url = $($("#sogou_vr_11002301_box_0 a")[0]).attr('href') || ''
+                let target = wechat_url.replace(/amp;/g, '')
+                resolve(target)
+            })
         })
     }
 }
 
-async function get(){
-//    let aa = await getUrl('南方都市报')
-    //console.log(aa)
-    await utils.getWechatUrl('南方都市报')
-}
-
-get()
 
 
 module.exports = utils
